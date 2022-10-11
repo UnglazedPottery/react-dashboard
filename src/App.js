ca02fa8b65d22dcf5a7c741b23ea86adcb4e901c
Jade Huynh <35jadeh@gmail.com>
2022-10-11 17:43:19 -0500

    setup context

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 import './App.css'
 
 const App = () => {
-    const activeMenu = false;
+    const { activeMenu } = useStateContext();
 
     return (
         <div>
@@ -42,8 +44,8 @@ const App = () => {
                     <div>
                         <Routes>
                             {/* Dashboard */}
-                            <Route path="/" element={<ECommerce />} />
-                            <Route path="/ecommerce" element={<ECommerce />} />
+                            <Route path="/" element={<Ecommerce />} />
+                            <Route path="/ecommerce" element={<Ecommerce />} />
 
                             {/* Pages */}
                             <Route path="/orders" element={<Orders />} />
